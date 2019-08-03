6f0b471e0a241dd94d38111b49c997ce253d04fe
oznelabs <israelwebti@gmail.com>
2019-08-03 08:42:34 -0300

    realizando a conecção com o mongodb com o mongoose

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,13 +1,11 @@
-const users = [
-  { id: 1, name: 'Israel', email: 'israelwebti@gmail.com' },
-  { id: 2, name: 'Giovana', email: 'giovana@gmail.com' },
-]
+const User = require('./User');
+
 module.exports = {
   Query: {
-    users: () => users,
-    user: () => users[0]
+    users: () => User.find(),
+    user: (_, { id }) => User.findById(id),
   },
   Mutation: {
-    createUser: () => users[0],
+    createUser: (_, { name, email }) => User.create({ name, email }),
   },
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const { GraphQLServer } = require('graphql-yoga');
 const path = require('path');
 const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://mongodb:Aderbal01@cluster0-ddknb.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+});
+
 const server = new GraphQLServer({
   typeDefs: path.resolve(__dirname, 'schema.graphql'),
   resolvers
